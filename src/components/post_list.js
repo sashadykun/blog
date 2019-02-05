f6b6e7241ce0a149198ad796da507c750094414f
Oleksandr Dykun <sashaluckyman@gmail.com>
2019-02-04 22:39:16 -0800

    implement middleware redux-thunk

---
+++ src/components/post_list.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts} from '../actions';
+
+
+class PostList extends Component {
+
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
+
+    render(){
+        return <div>Post List</div>
+    }
+}
+
+// const mapStateToProps = () =>{
+
+// }
+
+export default connect(null, {fetchPosts}) (PostList);
